dba663319b2344e7728c5cfe75ab0810aff4b3b2
savlik <savlik@MacBook-Pro-savlik.local>
2020-10-27 14:09:50 +0200

    to classnames

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import s from './App.modules.scss'
+import './custom.css'
+import cn from 'classnames'
+
+const App = () => {
+    return (
+        <div className={s.header + ' color'}>
+            <h1 >App? Hello</h1>
+        </div>
+    )
+}
+export default App
